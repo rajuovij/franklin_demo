d587d7d60f1e7c8216c9dfdb9baa15516adb2b33
rajuovij <raju.vijay.kumar88@gmail.com>
2023-04-07 14:21:55 +0530

    add corousel component

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -16,17 +16,41 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(ul);
 
-  const aNext = document.createElement('a');
+  /*const aNext = document.createElement('a');
   const aPrevious = document.createElement('a');
   aNext.className = 'next';
   aPrevious.className = 'prev';
   block.append(aNext);
-  block.append(aPrevious);
+  block.append(aPrevious);*/
 
   const carouselSlide = document.querySelector(".carousel ul");
   const carouselImages = document.querySelectorAll(".carousel-v1-image picture");
 
-  // Buttons
+  // Set up the automatic loop
+let slideIndex = 0;
+const slideInterval = setInterval(nextSlide, 10000);
+
+// Move the carousel to the next slide
+function nextSlide() {
+  if (slideIndex >= carouselImages.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  carouselSlide.style.transform = "translateX(-" + slideIndex * 100 + "%)";
+}
+
+// Stop the automatic loop when the user hovers over the carousel
+carouselSlide.addEventListener("mouseenter", () => {
+  clearInterval(slideInterval);
+});
+
+// Resume the automatic loop when the user stops hovering over the carousel
+carouselSlide.addEventListener("mouseleave", () => {
+  slideInterval = setInterval(nextSlide, 10000);
+});
+
+  /*// Buttons
   const prevBtn = document.querySelector(".prev");
   const nextBtn = document.querySelector(".next");
 
@@ -64,7 +88,7 @@ export default function decorate(block) {
       counter = carouselImages.length - counter;
       carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
     }
-  });
+  });*/
 
   
 }
