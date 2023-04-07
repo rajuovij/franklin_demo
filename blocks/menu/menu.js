ee1cb7826a67edaebddebd7ea159e154a123e926
rajuovij <raju.vijay.kumar88@gmail.com>
2023-04-07 09:35:01 +0530

    modifed menu.js

--- blocks/menu/menu.js
+++ blocks/menu/menu.js
@@ -3,15 +3,22 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  /*[...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
       div.className = 'menu-body';
     });
     ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  });*/
+
+  document.querySelectorAll('.block').forEach(block) => {
+    const li = document.createElement('li');
+    li.innerHTML = block.outerHTML;
+    ul.append(li);
+  }
+
+  //ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
