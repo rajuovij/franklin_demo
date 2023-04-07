2d693dbfb23019fd5a50ed2f3403eb17af4972ed
rajuovij <raju.vijay.kumar88@gmail.com>
2023-04-07 14:56:30 +0530

    modify corousel component to div

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -4,15 +4,15 @@ export default function decorate(block) {
   //const ul = document.createElement('div');
   //ul.className = 'carousel-v1-image';
   [...block.children].forEach((row) => {
-    //const li = document.createElement('div');
-    //li.innerHTML = row.innerHTML;
-    
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture'))  
+    const li = document.createElement('div');
+    li.innerHTML = row.innerHTML;
+    ul.className = 'carousel-v1-image';
+    /*[...li.children].forEach((div) => {
+      //if (div.children.length === 1 && div.querySelector('picture'))  
       div.innnerHTML = row;
       div.className = 'carousel-v1-image';
       
-    });
+    });*/
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
